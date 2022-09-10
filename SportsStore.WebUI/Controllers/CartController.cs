38f881e090cd15bca6d2f442cac32044805295f8
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-09-10 05:44:50 +0200

    added remove item

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -1,10 +1,9 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
-using SportsStore.WebUI.Models;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 namespace SportsStore.WebUI.Controllers
-
 {
     public class CartController : Controller
     {
@@ -13,46 +12,40 @@ namespace SportsStore.WebUI.Controllers
         {
             repository = repo;
         }
-
-        public ViewResult Index(string returnUrl)
+        public ViewResult Index(Cart cart, string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
-                ReturnUrl = returnUrl
+                ReturnUrl = returnUrl,
+                Cart = cart
             });
         }
-
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        public RedirectToRouteResult AddToCart(Cart cart, int productId,
+        string returnUrl)
         {
             Product product = repository.Products
             .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
-                GetCart().AddItem(product, 1);
+                cart.AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId,
+        string returnUrl)
         {
             Product product = repository.Products
             .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
-                GetCart().RemoveLine(product);
+                cart.RemoveLine(product);
             }
 
         return RedirectToAction("Index", new { returnUrl });
         }
-        private Cart GetCart()
+        public PartialViewResult Summary(Cart cart)
         {
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null)
-            {
-                cart = new Cart();
-                Session["Cart"] = cart;
-            }
-            return cart;
+            return PartialView(cart);
         }
     }
 }
