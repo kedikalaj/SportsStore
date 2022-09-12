3687ab7e9e1406e0aa121ad579c5e8d0f300eed0
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-09-13 01:54:07 +0200

    Added mobile specific views

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -11,19 +11,17 @@ namespace SportsStore.WebUI.Controllers
         {
             repository = repo;
         }
-        public PartialViewResult Menu(string category = null,
-        bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
-            ViewBag.SelectedCategory = category;
-            IEnumerable<string> categories = repository.Products
-            .Select(x => x.Category)
-           .Distinct()
-           .OrderBy(x => x);
+            {
+                ViewBag.SelectedCategory = category;
+                IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+               .Distinct()
+               .OrderBy(x => x);
 
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName, categories);
-
-            return PartialView(categories);
+                return PartialView("FlexMenu", categories);
+            }
         }
     }
 }
