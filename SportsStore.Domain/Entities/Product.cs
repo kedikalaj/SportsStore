7f5c8cf626b80ef4e2831b06163d8537d77f77a4
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-09-20 03:13:53 +0200

    Added image support for the database

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -21,5 +21,10 @@ namespace SportsStore.Domain.Entities
         public decimal Price { get; set; }
         [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
+
+
     }
 }
